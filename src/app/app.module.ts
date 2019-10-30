80d9e50f1be4b0ddefddf71c5c1467447c3ab059
Joe Giuffrida <giuffridajoe@gmail.com>
2019-10-30 15:25:49 -0400

    some css changes and new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
+/** modules are used to bundle diff functionaility of the application 
+ * 
+*/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  /** a selector can be defined a few ways:
+   * 1. referring to the element of the component itself
+   * 2. referring to an attrbiute of an html element
+   * 3. or by class
+   */
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  /** can declare a template URL (templateURL) or define the template in line within this file
+   *  use backticks instead of single quotes if defining template here*/
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
